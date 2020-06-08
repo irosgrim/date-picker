7d42eb2a4f96054de48bd0c70374a1b5cdf127e3
ion rosgrim <jrunin@gmail.com>
2020-06-08 21:52:00 +0200

    deploy to gh-pages, take 2

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/date-picker/dist/'
+    : '/',
+  devServer: {
+    overlay: {
+      warnings: false,
+      errors: false
+    }
+  },
+  lintOnSave: process.env.NODE_ENV !== 'production'
+}
